30b89e18ca15c32d97eda7d012f50e45fa2b8609
hienlq3 <hienlq3@fpt.com.vn>
2024-04-22 23:33:52 +0700

    rm comment PreAuthorize

--- src/main/java/com/fpt/fis/template/controller/rest/TemplateRestController.java
+++ src/main/java/com/fpt/fis/template/controller/rest/TemplateRestController.java
@@ -3,9 +3,7 @@ package com.fpt.fis.template.controller.rest;
 import com.fpt.fis.template.model.request.TemplateRequest;
 import com.fpt.fis.template.model.response.TemplateListFilterResponse;
 import com.fpt.fis.template.model.response.TemplateResponse;
-import com.fpt.fis.template.repository.entity.Template;
 import com.fpt.fis.template.service.TemplateService;
-import com.fpt.fis.template.utils.Constant;
 import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import org.springdoc.core.annotations.ParameterObject;
@@ -13,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -26,39 +23,39 @@ public class TemplateRestController {
     private TemplateService templateService;
 
     @GetMapping("{id}")
-//    @PreAuthorize("@author.hasPermission('template', 'view')")
+    @PreAuthorize("@author.hasPermission('template', 'view')")
     public Mono<TemplateResponse> readTemplateById(@Parameter @PathVariable("id") String id) {
         return templateService.readTemplateById(id);
     }
 
     @PostMapping()
-//    @PreAuthorize("@author.hasPermission('template', 'create')")
+    @PreAuthorize("@author.hasPermission('template', 'create')")
     public Mono<TemplateResponse> createTemplate(@RequestBody @Valid TemplateRequest request) {
         return templateService.createTemplate(request);
     }
 
     @PutMapping("{id}")
-//    @PreAuthorize("@author.hasPermission('template', 'update')")
+    @PreAuthorize("@author.hasPermission('template', 'update')")
     public Mono<TemplateResponse> updateTemplate(@Parameter @PathVariable("id") String id, @RequestBody @Valid TemplateRequest request) {
         return templateService.updateTemplate(id, request);
     }
 
     @GetMapping("")
-//    @PreAuthorize("@author.hasPermission('template', 'view')")
+    @PreAuthorize("@author.hasPermission('template', 'view')")
     public Mono<TemplateListFilterResponse> readAllTemplates(
-            @RequestParam(name = "query", required = false, defaultValue = Constant.BLANK) String searchText,
+            @RequestParam(name = "query", required = false) String searchText,
             @ParameterObject Pageable pageable) {
         return templateService.readAllTemplates(searchText, pageable);
     }
 
     @DeleteMapping("{id}")
-//    @PreAuthorize("@author.hasPermission('template', 'update')")
+    @PreAuthorize("@author.hasPermission('template', 'update')")
     public Mono<Void> deleteTemplate(@Parameter @PathVariable("id") String id) {
         return templateService.deleteTemplate(id);
     }
 
     @GetMapping("{id}/parameters")
-//    @PreAuthorize("@author.hasPermission('template', 'view')")
+    @PreAuthorize("@author.hasPermission('template', 'view')")
     public Mono<List<String>> getParamertersById(@Parameter @PathVariable("id") String id) {
         return templateService.getParamertersById(id);
     }
